a1a6580d312f86b9407fb665a74a71dd5b47129d
nicolluisyumang <yumangnicol@gmail.com>
2023-02-07 23:11:21 +0000

    feat(MultiplyInstruction): add MultiplyInstruction class that extends Instruction
    Add constructor and override execute(Machine), toString(), equals(Object), hashcode() methods.

---
+++ src/sml/instruction/MultiplyInstruction.java
@@ -0,0 +1,46 @@
+package sml.instruction;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.RegisterName;
+
+import java.util.Objects;
+
+public class MultiplyInstruction extends Instruction {
+    private final RegisterName result;
+    private final RegisterName source;
+
+    public static final String OP_CODE = "mul";
+
+    public MultiplyInstruction(String label, RegisterName result, RegisterName source) {
+        super(label, OP_CODE);
+        this.result = result;
+        this.source = source;
+    }
+
+    @Override
+    public int execute(Machine m) {
+        int value1 = m.getRegisters().get(result);
+        int value2 = m.getRegisters().get(source);
+        m.getRegisters().set(result, value1 * value2);
+        return NORMAL_PROGRAM_COUNTER_UPDATE;
+    }
+
+    @Override
+    public String toString() {
+        return getLabelString() + getOpcode() + " " + result + " " + source;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MultiplyInstruction that = (MultiplyInstruction) o;
+        return result.equals(that.result) && source.equals(that.source);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(result, source);
+    }
+}
