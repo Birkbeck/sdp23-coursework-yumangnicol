72575c3b86717b0700170615e24568620ac34bb3
nicolluisyumang <yumangnicol@gmail.com>
2023-02-08 20:29:06 +0000

    feat(JumpInstruction): add JumpInstruction class that extends Instruction
    Add constructor and override execute(Machine), toString(), equals(Object), hashcode() methods.

---
+++ src/sml/instruction/JumpInstruction.java
@@ -0,0 +1,45 @@
+package sml.instruction;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.RegisterName;
+
+import java.util.Objects;
+
+public class JumpInstruction extends Instruction {
+    private final RegisterName source;
+    private final String branch;
+    public static final String OP_CODE = "jnz";
+
+    public JumpInstruction(String label, RegisterName source, String branch) {
+        super(label, OP_CODE);
+        this.source = source;
+        this.branch = branch;
+    }
+
+    @Override
+    public int execute(Machine m) {
+        if (m.getRegisters().get(source) == 0) {
+            return NORMAL_PROGRAM_COUNTER_UPDATE;
+        }
+        return m.getLabels().getAddress(branch);
+    }
+
+    @Override
+    public String toString() {
+        return getLabelString() + getOpcode() + " " + source + " " + branch;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JumpInstruction that = (JumpInstruction) o;
+        return source.equals(that.source) && branch.equals(that.branch);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(source, branch);
+    }
+}
