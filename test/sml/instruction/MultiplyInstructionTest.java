56fe4a6d8bdd87105f1f24fea38a7b30c05c46d8
nicolluisyumang <yumangnicol@gmail.com>
2023-02-08 19:44:27 +0000

    test(MultiplyInstructionTest): add unit tests for MultiplyInstruction class

---
+++ test/sml/instruction/MultiplyInstructionTest.java
@@ -0,0 +1,48 @@
+package sml.instruction;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+
+import static sml.Registers.Register.*;
+
+public class MultiplyInstructionTest {
+    private Machine machine;
+    private Registers registers;
+
+    @BeforeEach
+    void setUp() {
+        machine = new Machine(new Registers());
+        registers = machine.getRegisters();
+        //...
+    }
+
+    @AfterEach
+    void tearDown() {
+        machine = null;
+        registers = null;
+    }
+
+    @Test
+    void executeValid() {
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        Instruction instruction = new MultiplyInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        Assertions.assertEquals(30, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void executeValidTwo() {
+        registers.set(EAX, -8);
+        registers.set(EBX, 9);
+        Instruction instruction = new MultiplyInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        Assertions.assertEquals(-72, machine.getRegisters().get(EAX));
+    }
+
+}
