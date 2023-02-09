de998a2bdb99320f58ff1bc8a0e92be13f1c7387
nicolluisyumang <yumangnicol@gmail.com>
2023-02-09 18:48:49 +0000

    feat(Labels): add implementations to Labels class methods
    Implement duplicate check on addLabel(String, int) method
    
    Implement NullPointerException check on getAddress(String) method
    
    Implement toString() method using stream
    
    Implement equals(Object) and hashCode() methods

--- src/sml/Labels.java
+++ src/sml/Labels.java
@@ -3,6 +3,7 @@ package sml;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 // TODO: write a JavaDoc for the class
 
@@ -21,8 +22,12 @@ public final class Labels {
 	 */
 	public void addLabel(String label, int address) {
 		Objects.requireNonNull(label);
-		// TODO: Add a check that there are no label duplicates.
-		labels.put(label, address);
+		// TODO: Add a check that there are no label duplicates. ✅
+		if(labels.containsKey(label)){
+			System.out.println("Label: " + label + " already exists");
+		} else {
+			labels.put(label, address);
+		}
 	}
 
 	/**
@@ -32,10 +37,21 @@ public final class Labels {
 	 * @return the address the label refers to
 	 */
 	public int getAddress(String label) {
-		// TODO: Where can NullPointerException be thrown here?
+		// TODO: Where can NullPointerException be thrown here? ✅
 		//       (Write an explanation.)
 		//       Add code to deal with non-existent labels.
-		return labels.get(label);
+
+		// the provided label might not exist in labels Map.
+		// in this case, a NullPointerException is thrown by the get() method.
+		// when a NullPointerException is thrown, the catch block handles this by returning
+		// -1 or the NORMAL_PROGRAM_COUNTER_UPDATE. The program should continue without breaking
+		// skipping the jump instruction, and moving to the next one.
+		try {
+			return labels.get(label);
+		} catch (NullPointerException e){
+			System.out.println("Label: " + label + " does not exist");
+			return -1;
+		}
 	}
 
 	/**
@@ -46,11 +62,28 @@ public final class Labels {
 	 */
 	@Override
 	public String toString() {
-		// TODO: Implement the method using the Stream API (see also class Registers).
-		return "";
+		// TODO: Implement the method using the Stream API (see also class Registers). ✅
+
+		return labels.entrySet().stream()
+				.sorted(Map.Entry.comparingByKey())
+				.map(e -> e.getKey() + " -> " + e.getValue())
+				.collect(Collectors.joining(", ", "[", "]"));
 	}
 
-	// TODO: Implement equals and hashCode (needed in class Machine).
+	// TODO: Implement equals and hashCode (needed in class Machine). ✅
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Labels labels1 = (Labels) o;
+		return labels.equals(labels1.labels);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(labels);
+	}
 
 	/**
 	 * Removes the labels
