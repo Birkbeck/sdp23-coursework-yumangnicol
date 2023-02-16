d6cbe6235a4132684c3e72c1b521932ff41dd011
nicolluisyumang <yumangnicol@gmail.com>
2023-02-16 23:18:42 +0000

    docs(Instruction): add Java documentation for Instruction class

--- src/sml/Instruction.java
+++ src/sml/Instruction.java
@@ -5,9 +5,11 @@ package sml;
 /**
  * Represents an abstract instruction.
  *
- * @author ...
+ * @author Nicol Luis Yumang
  */
 public abstract class Instruction {
+	public static int NORMAL_PROGRAM_COUNTER_UPDATE = -1;
+
 	protected final String label;
 	protected final String opcode;
 
@@ -23,15 +25,8 @@ public abstract class Instruction {
 		this.opcode = opcode;
 	}
 
-	public String getLabel() {
-		return label;
-	}
-
-	public String getOpcode() {
-		return opcode;
-	}
-
-	public static int NORMAL_PROGRAM_COUNTER_UPDATE = -1;
+	@Override
+	public abstract boolean equals(Object obj);
 
 	/**
 	 * Executes the instruction in the given machine.
@@ -41,26 +36,44 @@ public abstract class Instruction {
 	 *          or NORMAL_PROGRAM_COUNTER_UPDATE to indicate that
 	 *          the instruction with the next address is to be executed
 	 */
-
 	public abstract int execute(Machine machine);
 
+	public String getLabel() {
+		return label;
+	}
+
+	/**
+	 * Returns the unique label of the instruction suffixed with a colon (:)
+	 *
+	 * @return label
+	 */
 	protected String getLabelString() {
 		return (getLabel() == null) ? "" : getLabel() + ": ";
 	}
+
+	/**
+	 * Returns the operation code of an Instruction.
+	 *
+	 * @return operation code
+	 */
+	public String getOpcode() {
+		return opcode;
+	}
+
+	@Override
+	public abstract int hashCode();
 
 	// TODO: What does abstract in the declaration below mean? ✅
 	//       (Write a short explanation.)
 	// Abstract methods do not have a body or implementation. Only the method signature is provided
 	// Any class that contains an abstract method should also be an abstract class
 	// Any class that extends an abstract class requires an implementation the abstract methods of the extended class
+
+	/**
+	 * String representation of an Instruction
+	 *
+	 * @return pretty formatted version of an instruction
+	 */
 	@Override
 	public abstract String toString();
-
-	// TODO: Make sure that subclasses also implement equals and hashCode (needed in class Machine). ✅
-
-	@Override
-	public abstract int hashCode();
-
-	@Override
-	public abstract boolean equals(Object obj);
 }
