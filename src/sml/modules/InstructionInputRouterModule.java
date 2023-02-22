24cffc4dd2c8f4dc6daa80010d50a3bb34b1d91e
nicolluisyumang <yumangnicol@gmail.com>
2023-02-22 15:53:06 +0000

    docs(InstructionInputRouterModule): add Java documentation for InstructionInputRouterModule class

--- src/sml/modules/InstructionInputRouterModule.java
+++ src/sml/modules/InstructionInputRouterModule.java
@@ -6,6 +6,12 @@ import sml.InstructionFactory;
 import sml.InstructionInputRouter;
 import sml.utils.Registry;
 
+/**
+ * A Guice module that represents the bindings for InstructionInputRouter
+ * Each binding is a dependency being injected
+ *
+ * @author Nicol Luis Yumang
+ */
 public class InstructionInputRouterModule extends AbstractModule {
     protected void configure(){
         bind(InstructionInputRouter.class);
