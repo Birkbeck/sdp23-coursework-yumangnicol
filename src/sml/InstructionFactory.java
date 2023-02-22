5b57b4c7b8bdd8e96f774a34d40aa1f7b8691b6b
nicolluisyumang <yumangnicol@gmail.com>
2023-02-22 15:58:28 +0000

    docs(InstructionFactory): add Java documentation for InstructionFactory class

--- src/sml/InstructionFactory.java
+++ src/sml/InstructionFactory.java
@@ -1,5 +1,17 @@
 package sml;
 
+/**
+ * Represents an interface of a Factory of Machine instructions
+ * @author Nicol Luis Yumang
+ */
 public interface  InstructionFactory {
+    /**
+     * Returns an instance of a machine instruction created from the details
+     * of the translated program instruction
+     *
+     * @param label label of a program instruction
+     * @param operands operands used in the program instruction (can be registers, values, labels)
+     * @return an instance of a machine instruction
+     */
     Instruction create (String label, String[] operands);
 }
