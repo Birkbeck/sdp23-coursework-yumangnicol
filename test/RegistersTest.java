098f8ea967f4c18a2b71f60ac0c71d01c4b78b62
nicolluisyumang <yumangnicol@gmail.com>
2023-03-02 20:02:25 +0000

    test(RegistersTest): add additional unit tests to Registers class

--- test/RegistersTest.java
+++ test/RegistersTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import sml.Registers;
 
 import static sml.Registers.Register.EAX;
+import static sml.Registers.Register.EBX;
 
 public class RegistersTest {
 
@@ -31,6 +32,29 @@ public class RegistersTest {
         var other = new Registers();
         other.set(EAX, 1);
         Assertions.assertNotEquals(registers, other);
+    }
+
+    @Test
+    void clearValid() {
+        registers.set(EAX, 30);
+        registers.set(EBX, 200);
+        registers.clear();
+        Assertions.assertEquals(0, registers.get(EAX));
+        Assertions.assertEquals(0, registers.get(EBX));
+    }
+
+    @Test
+    void setAndGetValid() {
+        registers.set(EAX, 20);
+        int expected = 20;
+        Assertions.assertEquals(expected,registers.get(EAX));
+    }
+
+    @Test
+    void toStringValid() {
+        registers.set(EAX, 20);
+        String expected = "[EAX = 20, EBX = 0, ECX = 0, EDX = 0, ESP = 0, EBP = 0, ESI = 0, EDI = 0]";
+        Assertions.assertEquals(expected,registers.toString());
     }
 
 }
